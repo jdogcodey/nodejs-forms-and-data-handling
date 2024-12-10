0ba856c5d0a8175a7ac575ea499c43a14d4025a9
jdogcodey <jonnydawson@hotmail.com>
2024-12-10 15:17:33 +0000

    Copy code from tutorial

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,9 @@
 const usersStorage = require("../storages/usersStorage");
 const { body, validationResult } = require("express-validator");
 
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
 const validateUser = [
   body("firstName")
     .trim()
@@ -49,5 +52,36 @@ exports.usersCreateGet = (req, res) => {
 exports.usersCreatePost = (req, res) => {
   const { firstName, lastName } = req.body;
   usersStorage.addUser({ firstName, lastName });
+  res.redirect("/");
+};
+
+exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
+};
+
+exports.usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    res.redirect("/");
+  },
+];
+
+exports.usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
   res.redirect("/");
 };
