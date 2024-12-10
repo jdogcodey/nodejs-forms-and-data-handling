a1c6f6ade0063889811ba722e5fc891c158c22bd
jdogcodey <jonnydawson@hotmail.com>
2024-12-10 15:04:05 +0000

    Add code from tutorial

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,37 @@
 const usersStorage = require("../storages/usersStorage");
+const { body, validationResult } = require("express-validator");
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+];
+
+// We can pass an entire array of middleware validations to our controller.
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  },
+];
 
 exports.usersListGet = (req, res) => {
   res.render("index", {
