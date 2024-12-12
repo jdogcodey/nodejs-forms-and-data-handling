3bacfb81c0d96d4410640bdf74a1ecf9bc8dc682
jdogcodey <jonnydawson@hotmail.com>
2024-12-12 15:05:10 +0000

    Update Controllers with email, age, bio

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -49,8 +49,8 @@ exports.usersCreatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = req.body;
-    usersStorage.addUser({ firstName, lastName });
+    const { firstName, lastName, email, age, body } = req.body;
+    usersStorage.addUser({ firstName, lastName, email, age, body });
     res.redirect("/");
   },
 ];
@@ -88,8 +88,14 @@ exports.usersUpdatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = req.body;
-    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.updateUser(req.params.id, {
+      firstName,
+      lastName,
+      email,
+      age,
+      bio,
+    });
     res.redirect("/");
   },
 ];
